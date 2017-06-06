c35ce61bc46c1e4a8091940c61302455dd3905a1
saberopensource <by1419408386@gmail.com>
2017-06-06 10:38:59 +0800

    jformdesigner 添加插件
    这个插件无法应用一个主题 一直报错 无关紧要

---
+++ src/main/java/saberlife/socket/b/JFTest.java
@@ -0,0 +1,28 @@
+/*
+ * Created by JFormDesigner on Tue Jun 06 10:24:03 CST 2017
+ */
+
+package saberlife.socket.b;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author xiaobaisaber
+ */
+public class JFTest extends JPanel {
+    public JFTest() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        setLayout(new BorderLayout());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
